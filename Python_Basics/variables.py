cecfa49ed8822680fd8e5ba3daf090a7ca0047d5
tilaatalarry <hawaniyaki@gmail.com>
2024-10-28 08:51:30 -0700

    question 2

---
+++ Python_Basics/variables.py
@@ -0,0 +1,7 @@
+# Defining the variables
+name = "Hamdallah"      
+age = 30            
+city = "Accra"   
+
+# Printing the message using an f-string
+print(f"Hello, my name is {name}. I am {age} years old and I live in {city}.")
