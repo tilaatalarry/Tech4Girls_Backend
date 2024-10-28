5911bfedbe07c2af3c9e37afa53a9646ca202091
tilaatalarry <hawaniyaki@gmail.com>
2024-10-28 08:56:28 -0700

    question 3

---
+++ Python_Basics/string_methods.py
@@ -0,0 +1,17 @@
+# Demonstrating various string methods in Python
+
+# Example string
+original_string = "Hello World!"
+
+# Using upper() method
+print("Uppercase:", original_string.upper())
+
+# Using lower() method
+print("Lowercase:", original_string.lower())
+
+# Using replace() method
+print("Replaced:", original_string.replace("World", "Hamdallah"))
+
+# Using join() method
+words = ["Hello", "Hamdallah", "World"]
+print("Joined:", " ".join(words))
