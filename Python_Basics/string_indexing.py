9545d381ed4c00b84baff429ea99b00d5534f946
tilaatalarry <hawaniyaki@gmail.com>
2024-10-28 09:03:07 -0700

    question 5

---
+++ Python_Basics/string_indexing.py
@@ -0,0 +1,14 @@
+# Define the string variable
+sentence = "Python is powerful and easy to learn."
+
+# Print the length of the string
+print("Length of the sentence:", len(sentence))
+
+# Print the first character
+print("First character:", sentence[0])
+
+# Print the last character
+print("Last character:", sentence[-1])
+
+# Print the word "powerful"
+print("The word 'powerful':", sentence[10:18])
