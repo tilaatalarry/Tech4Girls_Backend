d0dfac2d0ecf6b07532d64ab6477bdbc859f8d6f
tilaatalarry <hawaniyaki@gmail.com>
2024-10-28 08:58:47 -0700

    question 4

---
+++ Python_Basics/f_string_expressions.py
@@ -0,0 +1,8 @@
+# Define the variables
+a = 10
+b = 20
+
+# Using an f-string to print the sum, difference, and product
+print(f"Sum of a and b: {a + b}")
+print(f"Difference of a and b: {a - b}")
+print(f"Product of a and b: {a * b}")
