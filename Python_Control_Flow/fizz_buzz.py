624705cecde73181e64fc00c4ebec39c3a15a519
tilaatalarry <hawaniyaki@gmail.com>
2024-10-30 10:34:51 -0700

    question 5

--- Python_Control_Flow/fizz_buzz.py
+++ Python_Control_Flow/fizz_buzz.py
@@ -1,16 +1,16 @@
 for i in range(1, 51):
 
- # if number is divisible by 3 and returns 0, print Fizz
-     if (i % 3 == 0):
-         print('Fizz')
+# if number is divisible by 3 and returns 0, print Fizz
+    if (i % 3 == 0):
+        print('Fizz')
 
- # if number is divisible by 5 and returns 0, print Buzz
-     elif (i % 5 == 0):
-         print('Buzz')
+# if number is divisible by 5 and returns 0, print Buzz
+    elif (i % 5 == 0):
+        print('Buzz')
 
 # if number is divisible by 3 and 5, print FizzBuzz
-     elif (i % 3 == 0 and i % 5 == 0):
-         print('FizzBuzz')
+    elif (i % 3 == 0 and i % 5 == 0):
+        print('FizzBuzz')
 
-     else:
-         print(i)
+    else:
+        print(i)
